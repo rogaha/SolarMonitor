eff68d41f25d0d0f0f215e01588f510d3d9992f0
sktzofrenic <dan@danwins.com>
2016-05-04 15:30:00 -0400

    Update to cookie-cutter app

---
+++ solartender/public/views.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""Public section, including homepage and signup."""
+from flask import Blueprint, flash, redirect, render_template, request, url_for
+from flask_login import login_required, login_user, logout_user
+
+from solartender.extensions import login_manager
+from solartender.public.forms import LoginForm
+from solartender.user.forms import RegisterForm
+from solartender.user.models import User
+from solartender.utils import flash_errors
+
+blueprint = Blueprint('public', __name__, static_folder='../static')
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    """Load user by ID."""
+    return User.get_by_id(int(user_id))
+
+
+@blueprint.route('/', methods=['GET', 'POST'])
+def home():
+    """Home page."""
+    form = LoginForm(request.form)
+    # Handle logging in
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            login_user(form.user)
+            flash('You are logged in.', 'success')
+            redirect_url = request.args.get('next') or url_for('user.members')
+            return redirect(redirect_url)
+        else:
+            flash_errors(form)
+    return render_template('public/home.html', form=form)
+
+
+@blueprint.route('/logout')
+@login_required
+def logout():
+    """Logout."""
+    logout_user()
+    flash('You are logged out.', 'info')
+    return redirect(url_for('public.home'))
+
+
+@blueprint.route('/register', methods=['GET', 'POST'])
+def register():
+    """Register new user."""
+    form = RegisterForm(request.form, csrf_enabled=False)
+    if form.validate_on_submit():
+        User.create(username=form.username.data, email=form.email.data, password=form.password.data, active=True)
+        flash('Thank you for registering. You can now log in.', 'success')
+        return redirect(url_for('public.home'))
+    else:
+        flash_errors(form)
+    return render_template('public/register.html', form=form)
+
+
+@blueprint.route('/about')
+def about():
+    """About page."""
+    form = LoginForm(request.form)
+    return render_template('public/about.html', form=form)
+
+@blueprint.route('/oauth', methods=['GET', 'POST'])
+def oauth():
+    """	The OAuth URL you provide here will be used to direct customers to your customer login page to complete the authorization."""
+    return render_template('public/oauth.html')
+
+@blueprint.route('/oauth-redirect', methods=['GET', 'POST'])
+def oauth_redirect():
+    """	The redirect URI you provide here is where PG&E will send the Authorization Code once customer authorization is completed and you make a request for the authorization code.
+    """
+    return render_template('public/oauth.html', page_title='Redirect')
