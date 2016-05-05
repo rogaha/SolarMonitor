be750ec5d16065a8f004222758039835dcec7e20
sktzofrenic <dan@danwins.com>
2016-05-05 15:10:48 -0400

    test

--- solarmonitor/public/views.py
+++ solarmonitor/public/views.py
@@ -109,5 +109,4 @@ def notifications():
     """	The URI you provide here is where PG&E will send notifications that customer-authorized data is available """
     if request.method == 'POST':
         print request.values
-        send_email('Dan <admin@solarmonitor>', 'dan@danwins.com', 'Incoming POST data', request.values )
     return render_template('public/oauth.html', page_title='Notification Bucket')
