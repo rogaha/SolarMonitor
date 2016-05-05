aa21830057ca6e62e0eed0e746bf26917500b561
sktzofrenic <dan@danwins.com>
2016-05-05 13:23:58 -0400

    Import login_user, logout_user

--- solarmonitor/extensions.py
+++ solarmonitor/extensions.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Extensions module. Each extension is initialized in the app factory located in app.py."""
 from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user, logout_user
 from flask_sqlalchemy import SQLAlchemy
 
 bcrypt = Bcrypt()
