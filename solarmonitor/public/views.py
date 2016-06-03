6585a3385b33fdece85c30c24c81c2d02c1cf5d8
sktzofrenic <dan@danwins.com>
2016-06-03 15:08:03 -0400

    t

--- solarmonitor/public/views.py
+++ solarmonitor/public/views.py
@@ -113,7 +113,7 @@ def test():
     session['resource_authorization'] = api.simple_request(
         'https://api.pge.com/GreenButtonConnect/espi/1_1/resource/Authorization',  session['client_credentials'][u'client_access_token'])
 
-    tree = ET.parse(session['resource_authorization'])
+    tree = ET.parse(session[u'resource_authorization'])
     root = tree.getroot()
     for resource in root.iter('resourceURI'):
         print resource
