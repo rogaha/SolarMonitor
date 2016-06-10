dda0add4700103299a0e208964a5950a4b7853e5
sktzofrenic <dan@danwins.com>
2016-06-10 13:06:28 -0400

    Undo Celery

--- solarmonitor/public/views.py
+++ solarmonitor/public/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """Public section, including homepage and signup."""
-from flask import Blueprint, flash, redirect, render_template, request, url_for, session, jsonify
+from flask import Blueprint, flash, redirect, render_template, request, url_for, session
 from flask_login import login_required, login_user, logout_user, current_user
 
 from solarmonitor.extensions import login_manager, db, login_user, logout_user
@@ -18,64 +18,6 @@ import xml.etree.cElementTree as ET
 import datetime
 from datetime import timedelta
 import pytz
-
-from solarmonitor.utils import celery
-from celery.task.control import inspect
-
-@celery.task(bind=True)
-def process_xml(self, xml):
-
-    reading_type = {}
-    data = parse(xml)
-    data.prettyprint()
-
-    for index, resource in enumerate(data[u'ns1:feed'][u'ns1:entry']):
-
-        if u'ns0:ReadingType' in resource[u'ns1:content']:
-            reading_type['commodity_type'] = resource[u'ns1:content'][u'ns0:ReadingType'][u'ns0:commodity']
-            reading_type['flow_direction'] = resource[u'ns1:content'][u'ns0:ReadingType'][u'ns0:flowDirection']
-            reading_type['unit_of_measure'] = resource[u'ns1:content'][u'ns0:ReadingType'][u'ns0:uom']
-            reading_type['measuring_period'] = resource[u'ns1:content'][u'ns0:ReadingType'][u'ns0:measuringPeriod']
-            reading_type['power_of_ten_multiplier'] = resource[u'ns1:content'][u'ns0:ReadingType'][u'ns0:powerOfTenMultiplier']
-            reading_type['accumulation_behavior'] = resource[u'ns1:content'][u'ns0:ReadingType'][u'ns0:accumulationBehaviour']
-
-        if u'ns0:IntervalBlock' in resource[u'ns1:content']:
-
-            for reading in resource[u'ns1:content'][u'ns0:IntervalBlock'][u'ns0:IntervalReading']:
-                reading_type['interval_start'] = reading[u'ns0:timePeriod'][u'ns0:start']
-                reading_type['interval_duration'] = reading[u'ns0:timePeriod'][u'ns0:duration']
-                reading_type['interval_value'] = reading[u'ns0:value']
-
-                usage_point = UsagePoint(
-                    user_id=50098,
-                    commodity_type=reading_type['commodity_type'],
-                    measuring_period=reading_type['measuring_period'],
-                    interval_value=reading_type['interval_value'],
-                    interval_start=datetime.datetime.fromtimestamp(int(reading_type['interval_start'])) - timedelta(hours=7),
-                    interval_duration=reading_type['interval_duration'],
-                    flow_direction=reading_type['flow_direction'],
-                    unit_of_measure=reading_type['unit_of_measure'],
-                    power_of_ten_multiplier=reading_type['power_of_ten_multiplier'],
-                    accumulation_behavior=reading_type['accumulation_behavior']
-                    )
-
-                duplicate_check = UsagePoint.query.filter_by(
-                    user_id=50098,
-                    interval_value=usage_point.interval_value,
-                    flow_direction=usage_point.flow_direction,
-                    interval_start=usage_point.interval_start
-                    ).first()
-
-                if duplicate_check == None:
-                    db.session.add(usage_point)
-                    db.session.commit()
-                else:
-                    print 'usage_point already in database'
-
-                self.update_state(state='PROGRESS',
-                                  meta={'current': index, 'total': len(data[u'ns1:feed'][u'ns1:entry'])})
-
-    return {'status': 'Task completed!'}
 
 
 
@@ -296,20 +238,7 @@ def charts(modify=None):
     hourly_combined_electric_usage = [x - y for x, y in zip(incoming_electric, outgoing_electric)]
 
 
-    return render_template('public/data_chart.html',
-        date_select_form=date_select_form,
-        download_data_form=download_data_form,
-        incoming_electric=incoming_electric,
-        outgoing_electric=outgoing_electric,
-        incoming_labels=incoming_labels,
-        outgoing_labels=outgoing_labels,
-        incoming_electric_daily_data=incoming_electric_daily_data,
-        incoming_electric_daily_label=incoming_electric_daily_label,
-        outgoing_electric_daily_data=outgoing_electric_daily_data,
-        outgoing_electric_daily_label=outgoing_electric_daily_label,
-        daily_combined_electric_usage=daily_combined_electric_usage,
-        hourly_combined_electric_usage=hourly_combined_electric_usage
-        )
+    return render_template('public/data_chart.html', date_select_form=date_select_form, download_data_form=download_data_form, incoming_electric=incoming_electric, outgoing_electric=outgoing_electric, incoming_labels=incoming_labels, outgoing_labels=outgoing_labels, incoming_electric_daily_data=incoming_electric_daily_data, incoming_electric_daily_label=incoming_electric_daily_label, outgoing_electric_daily_data=outgoing_electric_daily_data, outgoing_electric_daily_label=outgoing_electric_daily_label, daily_combined_electric_usage=daily_combined_electric_usage, hourly_combined_electric_usage=hourly_combined_electric_usage)
 
 @blueprint.route('/test')
 def test():
@@ -322,9 +251,6 @@ def test():
 @blueprint.route('/oauth', methods=['GET', 'POST'])
 def oauth():
     """	The OAuth URL you provide here will be used to direct customers to your customer login page to complete the authorization."""
-    i = inspect()
-    print i.scheduled()
-    print i.active()
     return render_template('public/oauth.html')
 
 @blueprint.route('/oauth-redirect', methods=['GET', 'POST'])
@@ -359,42 +285,53 @@ def notifications():
             """This for-loop will work through the bulk_data list containing one or more XML trees. It will parse the tree, and insert the useful parts into the
             database. Before calling db.session.commit(), we also check to see if the data is already in the system, and ignores the data if true.
             """
+            data = parse(resource['data'])
 
-            task = process_xml.delay((resource['data']))
+            data.prettyprint()
 
-            print "task id:", task.id
+            reading_type = {}
 
-    return render_template('public/oauth.html', page_title='Notification Bucket')
+            for resource in data[u'ns1:feed'][u'ns1:entry']:
 
-@blueprint.route('/status/<task_id>', methods=['GET', 'POST'])
-def taskstatus(task_id):
-    task = process_xml.AsyncResult(task_id)
-    if task.state == 'PENDING':
+                if u'ns0:ReadingType' in resource[u'ns1:content']:
+                    reading_type['commodity_type'] = resource[u'ns1:content'][u'ns0:ReadingType'][u'ns0:commodity']
+                    reading_type['flow_direction'] = resource[u'ns1:content'][u'ns0:ReadingType'][u'ns0:flowDirection']
+                    reading_type['unit_of_measure'] = resource[u'ns1:content'][u'ns0:ReadingType'][u'ns0:uom']
+                    reading_type['measuring_period'] = resource[u'ns1:content'][u'ns0:ReadingType'][u'ns0:measuringPeriod']
+                    reading_type['power_of_ten_multiplier'] = resource[u'ns1:content'][u'ns0:ReadingType'][u'ns0:powerOfTenMultiplier']
+                    reading_type['accumulation_behavior'] = resource[u'ns1:content'][u'ns0:ReadingType'][u'ns0:accumulationBehaviour']
 
-        response = {
-            'state': task.state,
-            'current': 0,
-            'total': 1,
-            'status': 'Pending...'
-        }
-    elif task.state != 'FAILURE':
-        response = {
-            'state': task.state,
-            'current': task.info.get('current', 0),
-            'total': task.info.get('total', 1),
-            'status': task.info.get('status', '')
-        }
-        if 'result' in task.info:
-            response['result'] = task.info['result']
-    else:
-        # something went wrong in the background job
-        response = {
-            'state': task.state,
-            'current': 1,
-            'total': 1,
-            'status': str(task.info),  # this is the exception raised
-        }
-    return jsonify(response)
+                if u'ns0:IntervalBlock' in resource[u'ns1:content']:
 
+                    for reading in resource[u'ns1:content'][u'ns0:IntervalBlock'][u'ns0:IntervalReading']:
+                        reading_type['interval_start'] = reading[u'ns0:timePeriod'][u'ns0:start']
+                        reading_type['interval_duration'] = reading[u'ns0:timePeriod'][u'ns0:duration']
+                        reading_type['interval_value'] = reading[u'ns0:value']
+
+                        usage_point = UsagePoint(
+                            user_id=50098,
+                            commodity_type=reading_type['commodity_type'],
+                            measuring_period=reading_type['measuring_period'],
+                            interval_value=reading_type['interval_value'],
+                            interval_start=datetime.datetime.fromtimestamp(int(reading_type['interval_start'])) - timedelta(hours=7),
+                            interval_duration=reading_type['interval_duration'],
+                            flow_direction=reading_type['flow_direction'],
+                            unit_of_measure=reading_type['unit_of_measure'],
+                            power_of_ten_multiplier=reading_type['power_of_ten_multiplier'],
+                            accumulation_behavior=reading_type['accumulation_behavior']
+                            )
+
+                        duplicate_check = UsagePoint.query.filter_by(
+                            user_id=50098,
+                            interval_value=usage_point.interval_value,
+                            flow_direction=usage_point.flow_direction,
+                            interval_start=usage_point.interval_start
+                            ).first()
+
+                        if duplicate_check == None:
+                            db.session.add(usage_point)
+                            db.session.commit()
+                        else:
+                            print 'usage_point already in database'
 
     return render_template('public/oauth.html', page_title='Notification Bucket')
