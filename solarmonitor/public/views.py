df9732dc734dc8f7b35d6deba3a2d48ed197bd9d
sktzofrenic <dan@danwins.com>
2016-06-13 07:07:17 -0400

    async

--- solarmonitor/public/views.py
+++ solarmonitor/public/views.py
@@ -28,6 +28,61 @@ def add(x, y):
     print y
     return x + y
 
+@celery.task(bind=True)
+def process_xml(self, xml):
+
+    reading_type = {}
+    data = parse(xml)
+    data.prettyprint()
+
+    for index, resource in enumerate(data[u'ns1:feed'][u'ns1:entry']):
+
+        if u'ns0:ReadingType' in resource[u'ns1:content']:
+            reading_type['commodity_type'] = resource[u'ns1:content'][u'ns0:ReadingType'][u'ns0:commodity']
+            reading_type['flow_direction'] = resource[u'ns1:content'][u'ns0:ReadingType'][u'ns0:flowDirection']
+            reading_type['unit_of_measure'] = resource[u'ns1:content'][u'ns0:ReadingType'][u'ns0:uom']
+            reading_type['measuring_period'] = resource[u'ns1:content'][u'ns0:ReadingType'][u'ns0:measuringPeriod']
+            reading_type['power_of_ten_multiplier'] = resource[u'ns1:content'][u'ns0:ReadingType'][u'ns0:powerOfTenMultiplier']
+            reading_type['accumulation_behavior'] = resource[u'ns1:content'][u'ns0:ReadingType'][u'ns0:accumulationBehaviour']
+
+        if u'ns0:IntervalBlock' in resource[u'ns1:content']:
+
+            for reading in resource[u'ns1:content'][u'ns0:IntervalBlock'][u'ns0:IntervalReading']:
+                reading_type['interval_start'] = reading[u'ns0:timePeriod'][u'ns0:start']
+                reading_type['interval_duration'] = reading[u'ns0:timePeriod'][u'ns0:duration']
+                reading_type['interval_value'] = reading[u'ns0:value']
+
+                usage_point = UsagePoint(
+                    user_id=50098,
+                    commodity_type=reading_type['commodity_type'],
+                    measuring_period=reading_type['measuring_period'],
+                    interval_value=reading_type['interval_value'],
+                    interval_start=datetime.datetime.fromtimestamp(int(reading_type['interval_start'])) - timedelta(hours=7),
+                    interval_duration=reading_type['interval_duration'],
+                    flow_direction=reading_type['flow_direction'],
+                    unit_of_measure=reading_type['unit_of_measure'],
+                    power_of_ten_multiplier=reading_type['power_of_ten_multiplier'],
+                    accumulation_behavior=reading_type['accumulation_behavior']
+                    )
+
+                duplicate_check = UsagePoint.query.filter_by(
+                    user_id=50098,
+                    interval_value=usage_point.interval_value,
+                    flow_direction=usage_point.flow_direction,
+                    interval_start=usage_point.interval_start
+                    ).first()
+
+                if duplicate_check == None:
+                    db.session.add(usage_point)
+                    db.session.commit()
+                else:
+                    print 'usage_point already in database'
+
+                self.update_state(state='PROGRESS',
+                                  meta={'current': index, 'total': len(data[u'ns1:feed'][u'ns1:entry'])})
+
+    return {'status': 'Task completed!'}
+
 local = pytz.timezone ('US/Eastern')
 
 config = Config()
@@ -292,53 +347,42 @@ def notifications():
             """This for-loop will work through the bulk_data list containing one or more XML trees. It will parse the tree, and insert the useful parts into the
             database. Before calling db.session.commit(), we also check to see if the data is already in the system, and ignores the data if true.
             """
-            data = parse(resource['data'])
 
-            data.prettyprint()
+            task = process_xml.delay((resource['data']))
 
-            reading_type = {}
+            print "task id:", task.id
 
-            for resource in data[u'ns1:feed'][u'ns1:entry']:
+    return render_template('public/oauth.html', page_title='Notification Bucket')
 
-                if u'ns0:ReadingType' in resource[u'ns1:content']:
-                    reading_type['commodity_type'] = resource[u'ns1:content'][u'ns0:ReadingType'][u'ns0:commodity']
-                    reading_type['flow_direction'] = resource[u'ns1:content'][u'ns0:ReadingType'][u'ns0:flowDirection']
-                    reading_type['unit_of_measure'] = resource[u'ns1:content'][u'ns0:ReadingType'][u'ns0:uom']
-                    reading_type['measuring_period'] = resource[u'ns1:content'][u'ns0:ReadingType'][u'ns0:measuringPeriod']
-                    reading_type['power_of_ten_multiplier'] = resource[u'ns1:content'][u'ns0:ReadingType'][u'ns0:powerOfTenMultiplier']
-                    reading_type['accumulation_behavior'] = resource[u'ns1:content'][u'ns0:ReadingType'][u'ns0:accumulationBehaviour']
+@blueprint.route('/status/<task_id>', methods=['GET', 'POST'])
+def taskstatus(task_id):
+    task = process_xml.AsyncResult(task_id)
+    if task.state == 'PENDING':
 
-                if u'ns0:IntervalBlock' in resource[u'ns1:content']:
+        response = {
+            'state': task.state,
+            'current': 0,
+            'total': 1,
+            'status': 'Pending...'
+        }
+    elif task.state != 'FAILURE':
+        response = {
+            'state': task.state,
+            'current': task.info.get('current', 0),
+            'total': task.info.get('total', 1),
+            'status': task.info.get('status', '')
+        }
+        if 'result' in task.info:
+            response['result'] = task.info['result']
+    else:
+        # something went wrong in the background job
+        response = {
+            'state': task.state,
+            'current': 1,
+            'total': 1,
+            'status': str(task.info),  # this is the exception raised
+        }
+    return jsonify(response)
 
-                    for reading in resource[u'ns1:content'][u'ns0:IntervalBlock'][u'ns0:IntervalReading']:
-                        reading_type['interval_start'] = reading[u'ns0:timePeriod'][u'ns0:start']
-                        reading_type['interval_duration'] = reading[u'ns0:timePeriod'][u'ns0:duration']
-                        reading_type['interval_value'] = reading[u'ns0:value']
-
-                        usage_point = UsagePoint(
-                            user_id=50098,
-                            commodity_type=reading_type['commodity_type'],
-                            measuring_period=reading_type['measuring_period'],
-                            interval_value=reading_type['interval_value'],
-                            interval_start=datetime.datetime.fromtimestamp(int(reading_type['interval_start'])) - timedelta(hours=7),
-                            interval_duration=reading_type['interval_duration'],
-                            flow_direction=reading_type['flow_direction'],
-                            unit_of_measure=reading_type['unit_of_measure'],
-                            power_of_ten_multiplier=reading_type['power_of_ten_multiplier'],
-                            accumulation_behavior=reading_type['accumulation_behavior']
-                            )
-
-                        duplicate_check = UsagePoint.query.filter_by(
-                            user_id=50098,
-                            interval_value=usage_point.interval_value,
-                            flow_direction=usage_point.flow_direction,
-                            interval_start=usage_point.interval_start
-                            ).first()
-
-                        if duplicate_check == None:
-                            db.session.add(usage_point)
-                            db.session.commit()
-                        else:
-                            print 'usage_point already in database'
 
     return render_template('public/oauth.html', page_title='Notification Bucket')
