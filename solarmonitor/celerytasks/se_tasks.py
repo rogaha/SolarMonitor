45eb6014a968180142bf2ad280fbe3c7b6b20698
sktzofrenic <dan@danwins.com>
2016-06-16 18:05:11 -0400

    Solar Edge with App Context

--- solarmonitor/celerytasks/se_tasks.py
+++ solarmonitor/celerytasks/se_tasks.py
@@ -9,23 +9,26 @@ from solarmonitor.utils import celery
 
 @celery.task(bind=True)
 def process_se_data(self, json_data):
+    from solarmonitor.app import create_app
+    app = create_app(ProdConfig)
+    with app.app_context():
 
-    for each in json_data['energy']['values']:
+        for each in json_data['energy']['values']:
 
-        usage_point = SolarEdgeUsagePoint()
-        usage_point.user_id = 1
-        usage_point.time_unit = json_data['energy']['timeUnit']
-        usage_point.unit_of_measure = json_data['energy']['unit']
-        usage_point.date = datetime.datetime.strptime(str(each['date']), '%Y-%m-%d %H:%M:%S')
-        if each['value'] == None:
-            usage_point.value = 0
-        else:
-            usage_point.value = each['value']
+            usage_point = SolarEdgeUsagePoint()
+            usage_point.user_id = 1
+            usage_point.time_unit = json_data['energy']['timeUnit']
+            usage_point.unit_of_measure = json_data['energy']['unit']
+            usage_point.date = datetime.datetime.strptime(str(each['date']), '%Y-%m-%d %H:%M:%S')
+            if each['value'] == None:
+                usage_point.value = 0
+            else:
+                usage_point.value = each['value']
 
-        duplicate_check = SolarEdgeUsagePoint.query.filter_by(date=usage_point.date, value=usage_point.value).first()
+            duplicate_check = SolarEdgeUsagePoint.query.filter_by(date=usage_point.date, value=usage_point.value).first()
 
-        if duplicate_check:
-            print 'duplicate usage point'
-        else:
-            db.session.add(usage_point)
-            db.session.commit()
+            if duplicate_check:
+                print 'duplicate usage point'
+            else:
+                db.session.add(usage_point)
+                db.session.commit()
