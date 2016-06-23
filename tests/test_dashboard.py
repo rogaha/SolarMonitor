aa52f92c8de0f50eb9dbd38d46779ad93ef1f95a
sktzofrenic <dan@danwins.com>
2016-06-23 15:54:39 -0400

    New Public Template
    Login/Register Form

--- tests/test_dashboard.py
+++ tests/test_dashboard.py
@@ -13,11 +13,7 @@ from solarmonitor.user.models import User
 class TestDashboard:
     """Tests for the user dashboard."""
 
-    def test_solar_edge_delete_data(self, user, testapp):
-        """Clicking "Clear database" in the solaredge tab, was clearing the entire
-        session. This resulted in the saved session data being cleared for the PGE
-        tab on the dashboard as well."""
-
+    def test_solar_edge_clear_session(self, user, testapp):
         # Goes to solar edge dashboard
         res = testapp.get(url_for('dashboard.solar_edge'))
 
@@ -32,7 +28,27 @@ class TestDashboard:
         assert 'value="2016-06-05"' in res
         assert 'value="2016-06-01"' in res
 
-        res = testapp.get(url_for('dashboard.solar_edge', modify='delete-data')).follow()
+        res = testapp.get(url_for('dashboard.solar_edge', modify='clear')).follow()
 
         assert 'value="2016-06-05"' not in res
         assert 'value="2016-06-01"' not in res
+
+    def test_pge_clear_session(self, user, testapp):
+        # Goes to pge dashboard
+        res = testapp.get(url_for('dashboard.charts'))
+
+        # Fills out date select form
+        form = res.forms['pge_data_view']
+        form['date_select_form-start_date'] = '2016-05-01'
+        form['date_select_form-start_date'] = '2016-05-31'
+        form['date_select_form-start_date'] = 'daily'
+        # Submits
+        res = form.submit().follow()
+
+        assert 'value="2016-05-01"' in res
+        assert 'value="2016-05-31"' in res
+
+        res = testapp.get(url_for('dashboard.charts', modify='clear')).follow()
+
+        assert 'value="2016-05-01"' not in res
+        assert 'value="2016-05-31"' not in res
