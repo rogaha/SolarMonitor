16562f93f36dc9942413a39e41e7c84680c8d5da
sktzofrenic <dan@danwins.com>
2016-09-07 19:24:40 -0400

    Add files

---
+++ solarmonitor/user/dashboard/forms.py
@@ -0,0 +1,9 @@
+"""Dashboard forms."""
+from flask_wtf import Form
+from wtforms import PasswordField, StringField, SubmitField, SelectField
+from wtforms.validators import Required, Length, Email
+
+class EventAddForm(Form):
+    date_time = StringField('Start Date')
+    info = StringField('Start Date')
+    event_type = SelectField('Time Unit', coerce=int, choices=[(1, 'Weather'), (2, 'Panel Related')])
