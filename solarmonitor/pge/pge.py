b0b6ec366729ded2c4c637d960bee7eaecc1ef13
sktzofrenic <dan@danwins.com>
2016-09-08 19:54:27 -0400

    Debug for OAuth Process

--- solarmonitor/pge/pge.py
+++ solarmonitor/pge/pge.py
@@ -125,7 +125,12 @@ class OAuth2:
 			energy_account.pge_refresh_token = res.get('refresh_token', None)
 			energy_account.pge_access_token = res.get('access_token', None)
 			energy_account.pge_subscription_id = subscription_id
-			energy_account.pge_usage_point = get_usage_point_from_xml(usage_point_xml.text)
+			try:
+				print usage_point_xml.text
+				energy_account.pge_usage_point = get_usage_point_from_xml(usage_point_xml.text)
+			except:
+				energy_account.pge_usage_point = 'Not found.'
+
 			db.session.commit()
 
 			return res
