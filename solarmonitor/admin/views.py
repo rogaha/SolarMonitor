250c3ac5260ef234dd56a7c072cd4c1693ad7007
sktzofrenic <dan@danwins.com>
2016-09-08 21:12:53 -0400

    Can now add and delete users

--- solarmonitor/admin/views.py
+++ solarmonitor/admin/views.py
@@ -1,10 +1,11 @@
 from flask import Blueprint, render_template, flash, redirect, url_for, session
-from flask_login import login_required, login_user, logout_user
+from flask_login import login_required, login_user, logout_user, current_user
 from solarmonitor.auth.forms import RegistrationForm
 from solarmonitor.utils import flash_errors, requires_roles, try_parsing_date
 from solarmonitor.user.models import User, EnergyAccount, AppEvent
 from solarmonitor.extensions import db
 from solarmonitor.public.forms import DownloadDataForm
+from solarmonitor.auth.forms import RegistrationForm
 
 from datetime import timedelta, datetime
 
@@ -14,20 +15,49 @@ blueprint = Blueprint('admin', __name__, url_prefix='/admin', static_folder='../
 
 @blueprint.route('/users', methods=['GET', 'POST'])
 @blueprint.route('/users/page/<int:page>', methods=['GET', 'POST'])
+@blueprint.route('/<modify>/<int:user_id>', methods=['GET', 'POST'])
 @login_required
 @requires_roles('Admin')
-def users(page=1):
+def users(page=1, modify=None, user_id=None):
     """Admin Home Page."""
     breadcrumbs = [('Admin Dashboard', 'dashboard', url_for('admin.users'))]
     heading = 'Admin Dashboard'
 
     users = User.query
 
+    add_user_form = RegistrationForm()
+
+    if modify == 'del':
+        user = User.query.filter_by(id=user_id).first()
+        if current_user == user:
+            flash('You can\'t delete yourself!')
+            return redirect(url_for('admin.users'))
+        db.session.delete(user)
+        db.session.commit()
+        flash('User deleted')
+        return redirect(url_for('admin.users'))
+
+    if add_user_form.validate_on_submit():
+        user = User(
+            email=add_user_form.email.data,
+            first_name=add_user_form.first_name.data,
+            last_name=add_user_form.last_name.data,
+            password=add_user_form.password.data,
+        )
+        energy_account = EnergyAccount(nick_name='Default Account')
+        user.energy_accounts.append(energy_account)
+        user.log_event(info="Default account created for new user.")
+        db.session.add(user)
+        db.session.commit()
+        flash('User created.', 'success')
+        return redirect(url_for('admin.users'))
+
     return render_template('admin/users.html',
         page=page,
         breadcrumbs=breadcrumbs,
         heading=heading,
         users=users.paginate(page, 10, False),
+        add_user_form=add_user_form
     )
 
 @blueprint.route('/events', methods=['GET', 'POST'])
