7676d388f4a764d62715182d9081c7fb62316726
sktzofrenic <dan@danwins.com>
2016-10-06 16:55:44 -0400

    Syntax error *grr*

--- solarmonitor/user/dashboard/views.py
+++ solarmonitor/user/dashboard/views.py
@@ -235,7 +235,7 @@ def charts(modify=None):
         start_date_pge = datetime.strptime(session['start_date_pge'], '%Y-%m-%d')
         end_date_pge = datetime.strptime(session['end_date_pge'], '%Y-%m-%d') + timedelta(days=1)
 
-        process_xml.delay(current_user.energy_accounts[0], start_date_pge, end_date_pge, , user_id=current_user.id)
+        process_xml.delay(current_user.energy_accounts[0], start_date_pge, end_date_pge, user_id=current_user.id)
         flash('Data processing')
         return redirect(url_for('dashboard.charts'))
 
