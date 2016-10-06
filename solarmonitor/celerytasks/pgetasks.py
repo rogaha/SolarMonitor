0dc5604a5d314350468b76f15371b830d3c82fde
sktzofrenic <dan@danwins.com>
2016-10-06 15:57:13 -0400

    token info

--- solarmonitor/celerytasks/pgetasks.py
+++ solarmonitor/celerytasks/pgetasks.py
@@ -21,6 +21,13 @@ def process_xml(self, energy_account, start_date, end_date):
 
         energy_account = EnergyAccount.query.filter_by(id=energy_account.id).first()
 
+        #Refresh the OAuth token. This token is good for 1 hour.
+        refresh_info = oauth.get_refresh_token(energy_account)
+        print refresh_info
+        energy_account.pge_refresh_token = refresh_info.get(u'refresh_token', None)
+        energy_account.pge_access_token = refresh_info.get(u'access_token', None)
+        db.session.commit()
+
         #pge_data is an XML document
         pge_data = api.sync_request(
             energy_account,
@@ -33,12 +40,7 @@ def process_xml(self, energy_account, start_date, end_date):
 
         if 'error' in pge_data:
             print pge_data['error']
-            #Refresh the OAuth token. This token is good for 1 hour.
-            refresh_info = oauth.get_refresh_token(energy_account)
-            print refresh_info
-            energy_account.pge_refresh_token = refresh_info.get(u'refresh_token', None)
-            energy_account.pge_access_token = refresh_info.get(u'access_token', None)
-            db.session.commit()
+            return
 
         reading_type = {}
         data = parse(pge_data)
