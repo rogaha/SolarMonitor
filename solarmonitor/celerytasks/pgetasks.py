c6f8db1972375fbdca4f4e3e44e4ebead1fb98a0
sktzofrenic <dan@danwins.com>
2016-10-06 20:56:52 -0400

    Make celery task accept date or datetime objects

--- solarmonitor/celerytasks/pgetasks.py
+++ solarmonitor/celerytasks/pgetasks.py
@@ -24,12 +24,14 @@ def backoff(attempts):
 
 @celery.task(bind=True, max_retries=16, soft_time_limit=86400)
 def process_xml(self, energy_account, start_date, end_date, user_id=1):
-    if not isinstance(start_date, datetime.date):
+    try:
         start_date = start_date.date()
-
-    if not isinstance(end_date, datetime.date):
+    except:
+        pass
+    try:
         end_date = end_date.date()
-
+    except:
+        pass
     from solarmonitor.app import create_app
     app = create_app(ProdConfig)
     with app.app_context():
