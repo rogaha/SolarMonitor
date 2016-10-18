6cea68d3d24d1260a707564da169c1df65939618
sktzofrenic <dan@danwins.com>
2016-10-18 06:39:18 -0400

    Fix PGE_last date bug

--- solarmonitor/celerytasks/pgetasks.py
+++ solarmonitor/celerytasks/pgetasks.py
@@ -24,6 +24,8 @@ def backoff(attempts):
 
 @celery.task(bind=True, max_retries=3, soft_time_limit=6000)
 def process_xml(self, energy_account, start_date, end_date, user_id=1):
+    if end_date > datetime.datetime.utcnow():
+        end_date = datetime.datetime.utcnow()
     from solarmonitor.app import create_app
     app = create_app(ProdConfig)
     with app.app_context():
