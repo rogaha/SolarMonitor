9159e69dcde0af47c2aacb61d48a6b51714edf04
sktzofrenic <dan@danwins.com>
2016-12-23 08:23:26 -0500

    Switch mailgun to use correct domain

--- solarmonitor/mailgun/mailgun_api.py
+++ solarmonitor/mailgun/mailgun_api.py
@@ -3,7 +3,7 @@ import requests
 
 def send_email(sender, subject, to, text):
     return requests.post(
-        "https://api.mailgun.net/v3/app618ef831f75f4a2eb15e3f08f18d09fe.mailgun.org/messages",
+        "https://api.mailgun.net/v3/solardatapros.com",
         auth=("api", "key-e3bfd2daee0cab79737c792954d54b12"),
         data={"from": sender,
               "to": to,
@@ -12,7 +12,7 @@ def send_email(sender, subject, to, text):
 
 def send_html_email(sender, subject, to, html):
     return requests.post(
-        "https://api.mailgun.net/v3/app618ef831f75f4a2eb15e3f08f18d09fe.mailgun.org/messages",
+        "https://api.mailgun.net/v3/solardatapros.com",
         auth=("api", "key-e3bfd2daee0cab79737c792954d54b12"),
         data={"from": sender,
               "to": to,
